d90c4e8f4a30f7a94675cd02bde388ab8bcb3c6e
Fernando Rocha <fhrlobacz@gmail.com>
2021-10-18 17:28:23 -0300

    testando novas maneiras

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 function handleSubmit(){
     nome = document.getElementById('it_nome').value
     email = document.getElementById('it_email').value
-    console.log(nome,email)
+    Email.send({
+        Host: host,
+        Username : sender,
+        Password : sender_password,
+        To : sender,
+        From : sender,
+        Subject : "Teste de implementação de formulário.",
+        Body : "Este e-mail foi enviado utilizando js e html.\n"+nome+"\n"+email,
+        }).then(
+            message => console.log(message)
+        );
 }
